b5a95e6bd8344d7eb13203014bf700402c21164b
joaquin <joaquin.ma41168@cesurformacion.com>
2022-03-04 11:11:15 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import pera from './imagenes/Pera.jpg';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [loading, setloading] = useState(true);
+  const [gato, setGato] = useState("Teko");
+  const [fruits, setFruits] = useState("");
 
+  useEffect(() => {
+    fetch("http://localhost:8080/fruits")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFruits(responseJson);
+        setloading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (loading) {
+    return <div>cargando...</div>;
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          {fruits.map((item) => (
+            <ul key={item.id}>
+              <li>{item.name}</li>
+              <li>{item.price}</li>
+              <li><img src={item.name}></img></li>
+              
+            
+          )
+            </ul>
+          ))}
+
+          <form>
+            <input type="submit" value="Submit" />
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
